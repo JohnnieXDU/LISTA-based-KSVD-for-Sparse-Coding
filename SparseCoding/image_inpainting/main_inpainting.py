c388d86ea24193cd34f6abd9efefae8fe3c7960e
YangJiao <yjiao.xdu@gmail.com>
2020-05-14 17:58:48 -0400

    add LSTM-LISTA

--- SparseCoding/image_inpainting/main_inpainting.py
+++ SparseCoding/image_inpainting/main_inpainting.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from image_inpainting import inpainting_omp, inpainting_ksvd, inpainting_ista, inpainting_lista
+from image_inpainting import inpainting_omp, inpainting_ksvd_omp, inpainting_ista, inpainting_lista, inpainting_ksvd_lista, inpainting_lstm
 from utils.utils_error import PSNR
 import warnings
 
@@ -16,14 +16,15 @@ if __name__ == '__main__':
     overlap = 3  # image patch overlap stride
     maxiter = 5
 
-    method = 'LISTA'
+    method = 'LSTM'
 
     if method == 'OMP':
-        img_ori, masked_img, recovered_img = inpainting_omp.inpainting_omp(missing_pixels, dicsize,
-                                                                           n_nonzero_coefs, blksize, overlap, maxiter, grayimg)
+        img_ori, masked_img, recovered_img = inpainting_omp.inpainting_omp(missing_pixels, dicsize, n_nonzero_coefs,
+                                                                           blksize, overlap, maxiter, grayimg)
 
     if method == 'KSVD_OMP':
-        img_ori, masked_img, recovered_img = inpainting_ksvd.inpainting_ksvd(missing_pixels, dicsize, n_nonzero_coefs, blksize, overlap, maxiter, grayimg)
+        img_ori, masked_img, recovered_img = inpainting_ksvd_omp.inpainting_ksvd(missing_pixels, dicsize, n_nonzero_coefs,
+                                                                                 blksize, overlap, maxiter, grayimg)
 
     if method == 'ISTA':
         img_ori, masked_img, recovered_img = inpainting_ista.inpainting_ista(missing_pixels, dicsize, n_nonzero_coefs,
@@ -32,8 +33,17 @@ if __name__ == '__main__':
         img_ori, masked_img, recovered_img = inpainting_lista.inpainting_lista(missing_pixels, dicsize, n_nonzero_coefs,
                                                                                blksize, overlap, maxiter, grayimg)
 
+    if method == 'KSVD_LISTA':
+        img_ori, masked_img, recovered_img = inpainting_ksvd_lista.inpainting_ksvd_lista(missing_pixels, dicsize, n_nonzero_coefs,
+                                                                                 blksize, overlap, maxiter, grayimg)
+
+    if method == 'LISTA_LSTM':
+        img_ori, masked_img, recovered_img = inpainting_lstm.inpainting_lstm(missing_pixels, dicsize, n_nonzero_coefs,
+                                                                                 blksize, overlap, maxiter, grayimg)
+
     # PSNR
     psnr = PSNR(img_ori, recovered_img)
+    print('PSNR={:.2f}'.format(psnr))
 
     if grayimg:
         plt.figure()
