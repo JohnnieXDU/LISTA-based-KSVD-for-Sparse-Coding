aaa2349bf10a31d6e486817656e737fa27e52828
YangJiao <yjiao.xdu@gmail.com>
2020-05-14 18:10:25 -0400

    update all 8 methods
    OMP/ISTA/LISTA/LISTA-LSTM and KSVD version

--- SparseCoding/image_inpainting/main_inpainting.py
+++ SparseCoding/image_inpainting/main_inpainting.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from image_inpainting import inpainting_omp, inpainting_ksvd_omp, inpainting_ista, inpainting_lista, inpainting_ksvd_lista, inpainting_lstm
+from image_inpainting import inpainting_omp, inpainting_ksvd_omp, inpainting_ista, inpainting_lista, \
+    inpainting_ksvd_lista, inpainting_lstm, inpainting_ksvd_ista, inpainting_ksvd_lstm
 from utils.utils_error import PSNR
 import warnings
 
@@ -16,15 +17,11 @@ if __name__ == '__main__':
     overlap = 3  # image patch overlap stride
     maxiter = 5
 
-    method = 'LISTA_LSTM'
+    method = 'KSVD_LISTA_LSTM'
 
     if method == 'OMP':
         img_ori, masked_img, recovered_img = inpainting_omp.inpainting_omp(missing_pixels, dicsize, n_nonzero_coefs,
                                                                            blksize, overlap, maxiter, grayimg)
-
-    if method == 'KSVD_OMP':
-        img_ori, masked_img, recovered_img = inpainting_ksvd_omp.inpainting_ksvd(missing_pixels, dicsize, n_nonzero_coefs,
-                                                                                 blksize, overlap, maxiter, grayimg)
 
     if method == 'ISTA':
         img_ori, masked_img, recovered_img = inpainting_ista.inpainting_ista(missing_pixels, dicsize, n_nonzero_coefs,
@@ -33,13 +30,26 @@ if __name__ == '__main__':
         img_ori, masked_img, recovered_img = inpainting_lista.inpainting_lista(missing_pixels, dicsize, n_nonzero_coefs,
                                                                                blksize, overlap, maxiter, grayimg)
 
+    if method == 'LISTA_LSTM':
+        img_ori, masked_img, recovered_img = inpainting_lstm.inpainting_lstm(missing_pixels, dicsize, n_nonzero_coefs,
+                                                                                 blksize, overlap, maxiter, grayimg)
+
+    if method == 'KSVD_OMP':
+        img_ori, masked_img, recovered_img = inpainting_ksvd_omp.inpainting_ksvd(missing_pixels, dicsize, n_nonzero_coefs,
+                                                                                 blksize, overlap, maxiter, grayimg)
+
+    if method == 'KSVD_ISTA':
+        img_ori, masked_img, recovered_img = inpainting_ksvd_ista.inpainting_ksvd_ista(missing_pixels, dicsize, n_nonzero_coefs,
+                                                                                 blksize, overlap, maxiter, grayimg)
+
     if method == 'KSVD_LISTA':
         img_ori, masked_img, recovered_img = inpainting_ksvd_lista.inpainting_ksvd_lista(missing_pixels, dicsize, n_nonzero_coefs,
                                                                                  blksize, overlap, maxiter, grayimg)
 
-    if method == 'LISTA_LSTM':
-        img_ori, masked_img, recovered_img = inpainting_lstm.inpainting_lstm(missing_pixels, dicsize, n_nonzero_coefs,
+    if method == 'KSVD_LISTA_LSTM':
+        img_ori, masked_img, recovered_img = inpainting_ksvd_lstm.inpainting_ksvd_lstm(missing_pixels, dicsize, n_nonzero_coefs,
                                                                                  blksize, overlap, maxiter, grayimg)
+
 
     # PSNR
     psnr = PSNR(img_ori, recovered_img)
